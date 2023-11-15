a79acf8b2bedb42a12b9ca0edf5184a4ef57d042
Deema Rain <deemarain@Deemas-MacBook-Pro.local>
2023-11-15 13:34:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+x = int(input(" give me 4 number that is even and doesnt end with a 0"))
+x =  x%2==0
+
+if x%2==0:
+    print("thank you")
+elif x < 2:
+    print("thats wrong")
+else:
+    print("thats also wrong")
+
+
+quit()
+
